568b173c7d69e92cc0188d7b205e4e83c1cc0fdb
Yussuf Ahmed <mtawolfroleplay@gmail.com>
2024-10-10 16:02:15 +0300

    ز

--- courseManagementService/src/main/java/com/lms/model/CourseModel.java
+++ courseManagementService/src/main/java/com/lms/model/CourseModel.java
@@ -1,8 +1,25 @@
 package com.lms.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+import java.sql.Timestamp;
+import java.util.UUID;
+
 @Data
-@
+@Table
 public class CourseModel {
+@Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+private String title;
+private String description;
+private String category;
+private UUID instructorId;
+private Enum status;
+private Timestamp created_at;
+private Timestamp updated_at;
 }
