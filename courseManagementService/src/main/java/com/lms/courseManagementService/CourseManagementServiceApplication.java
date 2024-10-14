576ac268d1ca7eb88c6b4c04d96ce817d5767248
mohamed <mohamed.mamdouh1861999@gmail.com>
2024-10-15 02:18:08 +0300

    update

--- courseManagementService/src/main/java/com/lms/courseManagementService/CourseManagementServiceApplication.java
+++ courseManagementService/src/main/java/com/lms/courseManagementService/CourseManagementServiceApplication.java
@@ -1,13 +1,42 @@
 package com.lms.courseManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@RestController
 public class CourseManagementServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CourseManagementServiceApplication.class, args);
+	@Autowired
+	private Leo_Repo repo;
+
+		@GetMapping("/{id}")
+		public lesson getlessons(@PathVariable("id") long id) {
+			return repo.findById(id).orElse(null);
+		}
+
+		@PostMapping("/lessons")
+		public lesson createlessons(@RequestBody lesson lesson) {
+			return repo.save(lesson);
+		}
+
+		@PutMapping("/{lessonid}")
+		public lesson updatelessons(@PathVariable("lessonid") long lessonId, @RequestBody lesson lesson) {
+		if (!repo.existsById(lessonId)) {
+			throw new ResourceNotFoundException("Lesson not found with id: " + lessonId);
+		}
+		lesson.setId(lessonId);
+		return repo.save(lesson);
+		}
+
+		@DeleteMapping("/{id}")
+		public void delete_lessons(@PathVariable("id") long id) {
+			repo.deleteById(id);
+		}
+
 	}
 
-}
+
+
+
+
+
